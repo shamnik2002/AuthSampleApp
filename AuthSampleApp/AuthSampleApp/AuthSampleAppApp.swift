be2bae98831e8eb6728fcfebe01c2bb046a413f5
Shamal Nikam <shamnik2002@gmail.com>
2026-01-08 22:45:06 -0500

    Project Created

---
+++ AuthSampleApp/AuthSampleApp/AuthSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthSampleAppApp.swift
+//  AuthSampleApp
+//
+//  Created by Shamal nikam on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct AuthSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthSampleApp/AuthSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthSampleApp
+//
+//  Created by Shamal nikam on 1/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
